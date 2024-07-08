5b76c9066d6a88b5689320b61ee19b81247406b9
Ericaruso <ericacaruso024@gmail.com>
2024-07-08 22:45:28 +0200

    Add Complete Exercise 10 React

--- 08-07-24_Es-react-10/esercizio-10-react/src/App.jsx
+++ 08-07-24_Es-react-10/esercizio-10-react/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import styles from "./App.module.css";
+
+const initialTodos = [
+  {
+  id: 1,
+  title: "Fare la spesa",
+  },
+  {
+    id: 2,
+    title: "Rinnovare assicurazione",
+    },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState(initialTodos);
+  const [input, setInput] = useState("");
+
+  const addTodo= () => {
+    if (!input.length) return;
+    setTodos([...todos, {id: todos.lenght + 1, title: input}])
+    setInput("");
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={styles.container}>
+      <input value={input} placeholder="ToDo" onChange={handleChange}/>
+      <button onClick={addTodo}>Add</button>
+
+      <div className={styles.container__list}> </div>
+        <h2>List to Do</h2>
+        <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <p>{todo.title}</p>
+            </li>
+          );
+        })}
+        </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
